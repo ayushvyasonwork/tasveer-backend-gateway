e00b763a20b55352a00555a8beb348776746f09f
Ayush Vyas <work.ayushvyas17@gmail.com>
2026-01-05 00:05:24 +0530

    fix: cors issue

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: "https://tasveer-one.vercel.app/",
+  origin: "https://tasveer-one.vercel.app",
 //   origin: process.env.REACT_FRONTEND_1,
 //   origin: "http://localhost:3000",
   credentials: true,
